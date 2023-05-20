fc145e3dedb2ca0464f116065b9130af30c51b3a
Pavan5527 <vadagampavan18@gmail.com>
2023-05-20 13:48:30 +0530

    Updated Review Block

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import BreadCrum from "../components/BreadCrum";
 import Meta from "../components/Meta";
 import { Link } from "react-router-dom";
 import ProductCard from "../components/ProductCard";
 import ReactStars from "react-rating-stars-component";
+import ReactImageZoom from "react-image-zoom";
 
 const SingleProduct = () => {
+  const props = {
+    width: 400,
+    height: 500,
+    zoomWidth: 500,
+    img: "https://cdn2.chrono24.com/images/uhren/14836757-s1zgt9htvausxng4p7nh22ey-ExtraLarge.jpg",
+  };
+  const [orderedProduct, setorderedProduct] = useState(true);
   return (
     <>
       <Meta title={"Product Name"} />
@@ -14,8 +22,19 @@ const SingleProduct = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-6">
-              <div className="col-6"></div>
+              <div className="main-product-image">
+                <div>
+                  <ReactImageZoom {...props} />
+                </div>
+              </div>
+              <div className="other-product-images d-flex flex-wrap gap-15">
+            <div><img src="https://cdn2.chrono24.com/images/uhren/14836757-s1zgt9htvausxng4p7nh22ey-ExtraLarge.jpg" className="img-fluid" alt="" /></div>
+            <div><img src="https://cdn2.chrono24.com/images/uhren/14836757-s1zgt9htvausxng4p7nh22ey-ExtraLarge.jpg" className="img-fluid" alt="" /></div>
+            <div><img src="https://cdn2.chrono24.com/images/uhren/14836757-s1zgt9htvausxng4p7nh22ey-ExtraLarge.jpg" className="img-fluid" alt="" /></div>
+            <div><img src="https://cdn2.chrono24.com/images/uhren/14836757-s1zgt9htvausxng4p7nh22ey-ExtraLarge.jpg" className="img-fluid" alt="" /></div>
+              </div>
             </div>
+            <div className="col-6"></div>
           </div>
         </div>
       </div>
@@ -23,8 +42,8 @@ const SingleProduct = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
+              <h4>Description</h4>
               <div className="bg-white p-3">
-                <h4>Description</h4>
                 <p>
                   A descriptive word is a word used to give details and more
                   information. Examples of descriptive words include colors,
@@ -35,31 +54,89 @@ const SingleProduct = () => {
           </div>
         </div>
       </div>
-      <div className="reviews-wrapper py-5 wish home-wrapper-2">
+      <section className="reviews-wrapper  wish home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
-              <div className="review-head">
+              <h3>Reviews</h3>
+              <div className="review-inner-wrapper">
                 <div className="review-head d-flex justify-content-between align-items-end">
                   <div>
                     <h4 className="mb-2">Customer Reviews</h4>
                     <div className="d-flex align-items-center gap-10">
-                    <ReactStars
-                      count={5}
-                      size={24}
-                      value={4}
-                      edit={false}
-                      activeColor="#ffd700"
-                    />
-                    <p className="mb-0">Based on 2 Reviews</p>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <p className="mb-0">Based on 2 Reviews</p>
                     </div>
+                  </div>
+                  {orderedProduct && (
+                    <div>
+                      <a
+                        className="text-dark text-decoration-underline"
+                        href=""
+                      >
+                        Write a Review
+                      </a>
+                    </div>
+                  )}
+                </div>
+                <div className="review-form py-4">
+                  <h4>Write a Review</h4>
+                  <form action="" className="d-flex flex-column gap-15">
+                    <div>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={true}
+                        activeColor="#ffd700"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        name=""
+                        id=""
+                        className="w-100 form-control"
+                        cols="30"
+                        rows="4"
+                        placeholder="Comments"
+                      ></textarea>
+                    </div>
+                    <div className="d-flex justify-content-end">
+                      <button className="button border-0">Submit Review</button>
+                    </div>
+                  </form>
+                </div>
+                <div className="reviews mt-4">
+                  <div className="review">
+                    <div className="d-flex gap-10 align-items-center">
+                      <h6 className="mb-0">Pavan</h6>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                    </div>
+                    <p className="mt-3">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Velit eius iure quaerat eos alias officia illo architecto
+                      commodi suscipit blanditiis fuga voluptatem aliquam,
+                      exercitationem ad! Odio magni totam cum ducimus!
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
       <section className="popular-wrapper wish py-5 home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
