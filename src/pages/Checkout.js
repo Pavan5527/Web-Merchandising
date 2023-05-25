247d5eb862c24495aa0cd14f432ed261cd0dbfdb
Pavan5527 <vadagampavan18@gmail.com>
2023-05-25 15:11:15 +0530

    updated cart page

---
+++ src/pages/Checkout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import BreadCrum from "../components/BreadCrum";
+import Meta from "../components/Meta";
+
+const Checkout = () => {
+  return (
+    <>
+      <div className="checkout-wrapper">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-7"></div>
+            <div className="col-5"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Checkout;
