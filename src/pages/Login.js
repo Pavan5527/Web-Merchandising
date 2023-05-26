045e83576d79b167f070c39ee8219a2f861ab7fe
Pavan5527 <vadagampavan18@gmail.com>
2023-05-26 21:16:59 +0530

    updated

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -27,7 +27,7 @@ const Login = () => {
                   <div>
                     <Link to="/forgot-password">Forgot Password?</Link>
                     <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
-                      <button  className="button border-0" type="submit">
+                      <button className="button border-0" type="submit">
                         Login
                       </button>
                       <Link to="/signup" className="button signup">
